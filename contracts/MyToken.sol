f8e89ac64154c914d3514cad1f98df4302d0187a
Vanshika310 <136240773+Vanshika310@users.noreply.github.com>
2023-08-10 00:01:16 +0530

    Create MyToken.sol

---
+++ contracts/MyToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NaturesBeauty is ERC721Enumerable, Ownable {
+
